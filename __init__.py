148f7622dc2f64789c63e07494d2e8b90246eb43
cristi1710 <162296096+cristi1710@users.noreply.github.com>
2026-03-04 23:38:01 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .butterworth import design_butterworth
-from .chebyshev import design_chebyshev1, design_chebyshev2
-from .cauer import design_cauer
-from .utils import prewarping, compute_frequency_response, check_specs
-from .cost_function import compute_J, rank_filters
